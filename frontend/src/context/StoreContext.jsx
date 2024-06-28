ea95f56ab5a4aa80753a211fc7b8695c5c992f07
Hau <gia.hau.nguyen.2004@gmail.com>
2024-06-28 15:52:17 +0700

    Complete frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -18,12 +18,24 @@ const StoreContextProvider = (props) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (let key in cartItems) {
+            if (cartItems[key] > 0) {
+                let itemInfo = food_list.find((product) => product._id == key)
+                totalAmount += itemInfo.price * cartItems[key]
+            }
+        }
+        return totalAmount
+    }
+
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
 
     useEffect(() => {
